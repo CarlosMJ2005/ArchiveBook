e7a1f796cf0b8c5975a69c6ca15cd1757db893bc
stevendam25-cpu <stevendam25@gmail.com>
2026-01-26 18:17:53 +0100

    Resolucion de problema

--- ArchiveBookAPI/archivebook/src/main/java/com/archivebook/archivebook/controller/LibroController.java
+++ ArchiveBookAPI/archivebook/src/main/java/com/archivebook/archivebook/controller/LibroController.java
@@ -22,11 +22,7 @@ public class LibroController {
         this.repository = repository;
     }
     
-<<<<<<< HEAD
      @GetMapping("/api/libros")
-=======
-    @GetMapping("/api/libros")
->>>>>>> origin/main
     public List<Libro> verLibros() {
         return repository.findAll();
     }
