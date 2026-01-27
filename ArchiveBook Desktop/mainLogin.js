0ed9946183566ffe9e6235cbf777e3c1ba66ee9d
isroro <145961229+isroro@users.noreply.github.com>
2026-01-27 20:47:30 +0100

    guardado de usuario completo y sistema me iconos completo

--- ArchiveBook Desktop/mainLogin.js
+++ ArchiveBook Desktop/mainLogin.js
@@ -1,22 +1,14 @@
 import { Controller } from './src/controller/controller.js';
 
+
 const controller = new Controller();
 
 window.onload = () => {
-         
-  // addEventListeners de la aplicacion
 
-  const emailLog = document.getElementById("email-log")
-  const passwordlog = document.getElementById("password-log")
-
-  const emailSign = document.getElementById("email-sign")
-  const passwordSign = document.getElementById("password-sign")
-  const confirmationSign = document.getElementById("confirmation-sign")
-
-  document.getElementById("login").addEventListener('click', () => controller.login(emailLog,passwordlog));
-  document.getElementById("signin").addEventListener('click', () => controller.signin(emailSign,passwordSign,confirmationSign));
+  document.getElementById("login").addEventListener('click', () => controller.login());
+  document.getElementById("signin").addEventListener('click', () => controller.signin());
   document.getElementById("log-to-sign").addEventListener('click', () => controller.change(true));
-  document.getElementById("sign-to-log").addEventListener('click', () => controller.change(false));
+  document.getElementById("sign-to-log").addEventListener('click', () => controller.change());
   
   controller.init();
 }
--- ArchiveBook Desktop/src/view/loginView.js
+++ ArchiveBook Desktop/src/view/loginView.js
@@ -1,11 +1,73 @@
 export class loginView {
+    #emailLog
+    #passwordlog
+    #emailSign
+    #passwordSign
+    #confirmationSign
+    #structureLog
+    #structureSign
+    #stateCheckbox
 
     constructor() {
+        this.#emailLog = document.getElementById("email-log")
+        this.#passwordlog = document.getElementById("password-log")
+
+        this.#emailSign = document.getElementById("email-sign")
+        this.#passwordSign = document.getElementById("password-sign")
+        this.#confirmationSign = document.getElementById("confirmation-sign")
+
+        this.#structureLog = document.getElementById('log')
+        this.#structureSign = document.getElementById('sign')
+
+        this.#stateCheckbox = document.getElementById('state')
     }
 
     init() {
     }
     showError(){
         console.log("Bua, la has cagado")
+    }
+    reset(){
+        this.#emailLog.value = ""
+        this.#passwordlog.value = ""
+    }
+    fulfill(email, password, state){
+        this.#emailLog.value = email
+        this.#passwordlog.value = password
+        console.log("por defecto" + this.#stateCheckbox.value)
+        console.log("externo" + state)
+        this.#stateCheckbox.checked = state
+        console.log("tras cambio" + this.#stateCheckbox.value)
+    }
+    change(bool = false) {
+        console.log("cambio entre tipos")
+        if (bool) {
+            console.log("muestro sign")
+            this.#structureSign.classList.remove("d-none")
+            this.#structureLog.classList.add("d-none")
+        }
+        else {
+            console.log("muestro log")
+            this.#structureLog.classList.remove("d-none")
+            this.#structureSign.classList.add("d-none")
+        }
+    }
+    getEmailLog(){
+        return this.#emailLog.value
+    }
+    getPasswordLog(){
+        return this.#passwordlog.value
+    }
+    getEmailSign(){
+        return this.#emailSign.value
+    }
+    getPasswordSign(){
+        return this.#passwordSign.value
+    }
+    getConfirmationSign(){
+        return this.#confirmationSign.value
+    }
+    getStateCheckbox(){
+        return this.#stateCheckbox.checked
     }
 }
